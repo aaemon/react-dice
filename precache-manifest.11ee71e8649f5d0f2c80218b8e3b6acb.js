aeb5fa236227caf0a95e3a21c3b969b4a3c4cba9
Ammer Ashraf Emon <aaemon@ip-172-31-74-22.ec2.internal>
2020-06-11 12:32:45 +0000

    Updates

---
+++ precache-manifest.11ee71e8649f5d0f2c80218b8e3b6acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1cf073836df2159f6562816049db37",
+    "url": "/react-dice/index.html"
+  },
+  {
+    "revision": "8f6da83c0a8914ab9089",
+    "url": "/react-dice/static/css/main.7d784fa6.chunk.css"
+  },
+  {
+    "revision": "9c91bbf6221f93b8fdc4",
+    "url": "/react-dice/static/js/2.f7740c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice/static/js/2.f7740c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6da83c0a8914ab9089",
+    "url": "/react-dice/static/js/main.d6516a55.chunk.js"
+  },
+  {
+    "revision": "533d24086c14866049e5",
+    "url": "/react-dice/static/js/runtime-main.c076d6a6.js"
+  }
+]);
